7cdce2fc3debc38ad0867b56ac5d2ffbffae79f9
Rathan8 <srirathan.vlit@gmail.com>
2021-06-17 08:54:23 -0700

    installing packages in user directory

--- cloudshell-cqlsh_integration_sigv4/setup.sh
+++ cloudshell-cqlsh_integration_sigv4/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 echo " Download the Starfield digital certificate to connect using SSL/TLS"
 curl https://certs.secureserver.net/repository/sf-class2-root.crt --output  ~/keyspaces.crt
-export PATH=$PATH:amazon-keyspaces-toolkit/cqlsh-expansion/bin
+echo "Setting up python2 as default python"
+echo "alias python='/usr/bin/python2'" >>  ~/.bashrc
+source ~/.bashrc
+echo " Downloading & installing pip2 for python in home directory"
 curl -LO https://bootstrap.pypa.io/pip/2.7/get-pip.py
 python get-pip.py --user
-sudo pip2 install --user cassandra-sigv4
+echo "Installing cassandra-sigv4 using pip2 in home directory"
+pip2 install --user cassandra-sigv4
+echo "Setting up path for cqlsh-expansion"
+export PATH=$PATH:~/amazon-keyspaces-toolkit/cloudshell-cqlsh_integration_sigv4/bin
+echo "Copying cqlshrc for cassandra user"
 cp ~/amazon-keyspaces-toolkit/cloudshell-cqlsh_integration_sigv4/cqlshrc ~/.cassandra/cqlshrc
