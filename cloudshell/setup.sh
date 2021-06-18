6446bcf19d326292c3f72b9ec89c80b225ea9256
Rathan8 <srirathan.vlit@gmail.com>
2021-06-18 08:28:53 -0700

    formatted script

--- cloudshell/setup.sh
+++ cloudshell/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# echo "Setting up python2 as default python"
-# echo "alias python='/usr/bin/python2'" >>  ~/.bashrc
-# source ~/.bashrc
-echo " Creating cassandra Dir"
+echo "Creating cassandra Dir"
 mkdir ~/.cassandra
-echo " Downloading & installing pip2 for python2 in home directory"
+
+echo "Downloading & installing pip2 for python2 in home directory"
 curl -L https://bootstrap.pypa.io/pip/2.7/get-pip.py  -o ~/.cassandra/get-pip.py
 python2 ~/.cassandra/get-pip.py --user
+
 echo "Installing cqlsh & cassandra-sigv4 using pip2 in home directory"
 pip2 install --user cassandra-sigv4
 pip2 install --user cqlsh
+
 echo "downloading cqlsh-expansion files & moving them to local user bin directory"
 wget https://raw.githubusercontent.com/aws-samples/amazon-keyspaces-toolkit/master/cqlsh-expansion/bin/cqlsh-expansion -P /home/cloudshell-user/.local/bin
 chmod +x /home/cloudshell-user/.local/bin/cqlsh-expansion
@@ -19,9 +19,8 @@ wget https://raw.githubusercontent.com/aws-samples/amazon-keyspaces-toolkit/mast
 echo "Copying cqlshrc for cassandra user"
 wget  https://raw.githubusercontent.com/aws-samples/amazon-keyspaces-toolkit/master/cqlshrc -P ~/.cassandra
 
-echo " Download the Starfield digital certificate to connect using SSL/TLS"
+echo "Download the Starfield digital certificate to connect using SSL/TLS"
 curl https://certs.secureserver.net/repository/sf-class2-root.crt --output  ~/.cassandra/keyspaces.crt
-
 sed -i 's;certfile = .*$;certfile = ~/.cassandra/keyspaces.crt;g' ~/.cassandra/cqlshrc
 
 echo "Cleaning up pip2 cache & remove unneccesary files"
