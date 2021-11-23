5a7087190496fb16ad479a8758b020e6e1ef9618
Autumn <34965675+Autumn808@users.noreply.github.com>
2021-11-23 12:35:03 -0800

    Update from docker to Python package
    This script will now use the Keyspaces toolkit that utilizes the Python Package instead of having to use Docker.

--- aws-cloudshell/setup.sh
+++ aws-cloudshell/setup.sh
@@ -17,42 +17,32 @@
 
 
 
+echo "Creates cassandra Directory if it doesn't exists"
 
-#echo "Creates cassandra Directory if it doesn't exists"
-```mkdir -p ~/.cassandra```
+mkdir -p ~/.cassandra
 
-#echo "Downloading & installing pip2 for python2 in home directory"
-```curl -L https://bootstrap.pypa.io/pip/2.7/get-pip.py -o ~/.cassandra/get-pip.py
-python2 ~/.cassandra/get-pip.py —user```
+echo "Downloading & installing pip2 for python2 in home directory"
 
-
-
-To install the cqlsh-expansion python package you can run the following pip2 command.
-The command below executes a “pip2 install” that will install the cqlsh-expansion scripts. 
-It will also install a requirements file containing a list of dependencies. 
-The --user flag tells pip to use the Python user install directory for your platform. 
-
-Typically ~/.local/ on unix based systems.
-```pip2 install —user cqlsh-expansion```
+curl -L https://bootstrap.pypa.io/pip/2.7/get-pip.py -o ~/.cassandra/get-pip.py
+python2 ~/.cassandra/get-pip.py —user
 
 
 
-#Setup cqlsh-expansion to connect to Amazon Keyspaces
+# To install the cqlsh-expansion python package executing the following pip2 command. 
+
+echo “Installing cqlsh-expansion "
+pip2 install —user cqlsh-expansion
 
 
-#To use the cqlsh-expansion with Amazon Keyspaces you can use the following post install script or by following the instructions found in the official Amazon Keyspaces documentation.
+# To configure cqlsh-expanson 
 
-By default the cqlsh-expansion is not configured with ssl enabled, but the package includes a post install script helper to quickly setup your environment after installation. 
-The script will place the necessary configuration and SSL certificate in the user’s .cassandra directory. Amazon Keyspaces only accepts secure connections using Transport Layer Security (TLS). 
-Encryption in transit provides an additional layer of data protection by encrypting your data as it travels to and from Amazon Keyspaces. The post install script first will create the .cassandra directory if it does not exist already. 
-Then it will copy a preconfigure a cqlshrc file and the Starfield digital certificate into the .cassandra directory. The .cassandra directory will be created in the user home directory as it is the default location. As best practice, please review the post install script before executing. Modifications made by this post install script will not be undone if uninstalling the cqlsh-expansion with pip.
-
-```cqlsh-expansion.init```
+echo “configuring cqlsh-expansion to connect to Amazon Keyspaces”
+cqlsh-expansion.init
 
 
 
-#echo "Cleaning up pip2 cache & remove unnecessary files"
+echo "Cleaning up pip2 cache & remove unnecessary files"
 
-```pip2 cache purge
+pip2 cache purge
 rm -f ~/.cassandra/get-pip.py
-pip2 uninstall cqlsh-expansion```
+pip2 uninstall cqlsh-expansion
