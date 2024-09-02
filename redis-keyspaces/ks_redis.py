93f7fc09c52f4190f2841bfc4bfd89ae49079d27
EC2 Default User <ec2-user@ip-172-31-45-213.ec2.internal>
2024-09-02 11:07:04 +0000

    Changed method of authentication to sigv4

--- redis-keyspaces/ks_redis.py
+++ redis-keyspaces/ks_redis.py
@@ -1,32 +1,33 @@
 from cassandra.cluster import *
 from ssl import SSLContext, PROTOCOL_TLSv1_2 , CERT_REQUIRED
 from cassandra.auth import PlainTextAuthProvider
+from cassandra_sigv4.auth import SigV4AuthProvider
+from cassandra.query import SimpleStatement
 from rediscluster import RedisCluster
 import logging
 import time
+import boto3
 
 #Keyspaces connection
 ssl_context = SSLContext(PROTOCOL_TLSv1_2)
 ssl_context.load_verify_locations('/home/ec2-user/sf-class2-root.crt')
 ssl_context.verify_mode = CERT_REQUIRED
-exec_profile = ExecutionProfile(consistency_level=ConsistencyLevel.LOCAL_QUORUM, 
-                                load_balancing_policy=DCAwareRoundRobinPolicy())
-auth_provider = PlainTextAuthProvider(username='YOUR USERNAME', 
-                                      password='YOUR PASSWORD')
+boto_session = boto3.Session()
+auth_provider = SigV4AuthProvider(boto_session)
 cluster = Cluster(['cassandra.us-east-1.amazonaws.com'], 
                   ssl_context=ssl_context, 
-                  auth_provider=auth_provider, 
-                  port=9142, 
-                  execution_profiles={EXEC_PROFILE_DEFAULT: exec_profile})
+                  auth_provider=auth_provider,
+                  port=9142)
 session = cluster.connect()
 
 
 #Amazon Elasticache connection
 logging.basicConfig(level=logging.ERROR)
-redis = RedisCluster(startup_nodes=[{"host": "cluster_identifier.xxxxx.clustercfg.use1.cache.amazonaws.com", 
+redis = RedisCluster(startup_nodes=[{"host": "keyspaces-cache.ebnqkc.clustercfg.use1.cache.amazonaws.com",
                                      "port": "6379"}], 
                      decode_responses=True,
                      skip_full_coverage_check=True)
+
 if redis.ping():
     logging.info("Connected to Redis")
 
@@ -42,15 +43,15 @@ def write_award(book_award):
 
 #write row to the Keyspaces table
 def write_award_to_keyspaces(book_award):
-    stmt = session.prepare(f"INSERT INTO {keyspace_name}.{table_name} (award, year, category, rank, author, book_title, publisher) VALUES (?, ?, ?, ?, ?, ?, ?);")
-    session.execute(stmt, 
-                    parameters=[book_award["award"], 
+    stmt=SimpleStatement(f"INSERT INTO {keyspace_name}.{table_name} (award, year, category, rank, author, book_title, publisher) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                         consistency_level=ConsistencyLevel.LOCAL_QUORUM)
+    session.execute(stmt,(book_award["award"], 
                                 book_award["year"], 
                                 book_award["category"], 
                                 book_award["rank"], 
                                 book_award["author"], 
                                 book_award["book_title"], 
-                                book_award["publisher"]])
+                                book_award["publisher"]))
 
 #write row to the cache
 def write_award_to_cache(book_award):
@@ -68,8 +69,9 @@ def delete_award(award, year, category, rank):
 
 #delete row from Keyspaces table
 def delete_award_from_keyspaces(award, year, category, rank):
-    stmt = session.prepare(f"DELETE FROM {keyspace_name}.{table_name} WHERE award=? AND year=? AND category=? AND rank=?;")
-    session.execute(stmt, parameters=[award, int(year), category, int(rank)])
+    stmt = SimpleStatement(f"DELETE FROM {keyspace_name}.{table_name} WHERE award=%s AND year=%s AND category=%s AND rank=%s;",
+                           consistency_level=ConsistencyLevel.LOCAL_QUORUM)
+    session.execute(stmt, (award, int(year), category, int(rank)))
 
 #delete row from cache
 def delete_award_from_cache(award, year, category, rank):
@@ -89,8 +91,8 @@ def get_award(award, year, category, rank):
     #if row not in cache, fetch it from Keyspaces table
     if not book_award:
         print("Fetched from Cache: ", book_award)
-        stmt = session.prepare(f"SELECT * FROM {keyspace_name}.{table_name} WHERE award=? AND year=? AND category=? AND rank=?;")
-        res=session.execute(stmt, parameters=[award, int(year), category, int(rank)])
+        stmt = SimpleStatement(f"SELECT * FROM {keyspace_name}.{table_name} WHERE award=%s AND year=%s AND category=%s AND rank=%s;")
+        res=session.execute(stmt, (award, int(year), category, int(rank)))
         if not res.current_rows:
             print("Fetched from Database: None")
             return None
@@ -115,7 +117,7 @@ def get_awards(parameters):
     #if result set not in cache, fetch it from Keyspaces table
     if not book_awards:
         print("Fetched from Cache: ", book_awards)
-        stmt = session.prepare(f"SELECT * FROM {keyspace_name}.{table_name} WHERE award=? AND year=? AND category=? AND rank<=?;")
+        stmt = SimpleStatement(f"SELECT * FROM {keyspace_name}.{table_name} WHERE award=%s AND year=%s AND category=%s AND rank<=%s;")
         res=session.execute(stmt, parameters)
         if not res.current_rows:
             print("Fetched from Database: None")
